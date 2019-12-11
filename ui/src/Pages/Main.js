98eba5dbc91cc3a31a0fd27743b8dd5c02e5ca61
Eric Lu <eric.lu02@sap.com>
2019-12-11 19:50:54 +0800

    Add topic selection page

--- ui/src/Pages/Main.js
+++ ui/src/Pages/Main.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 import Welcome from './Welcome';
+import TopicSelection from './TopicSelection';
 
 function Main() {
   return (
     <Router>
       <Route path="/welcome/" exact component={Welcome} />
+      <Route path="/topic/" exact component={TopicSelection} />
     </Router>
   )
 }
---
+++ ui/src/Pages/TopicSelection.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import { NavBar, WhiteSpace, WingBlank, Flex, Grid } from 'antd-mobile';
+import { Avatar, Icon } from 'antd';
+import 'antd-mobile/dist/antd-mobile.css';
+import 'antd/dist/antd.css';
+import '../static/css/TopicSelection.css';
+
+class Test extends Component {
+
+  render() {
+
+    const dataList = [
+      {icon: 'home', text: '租房'},
+      {icon: 'car', text: '拼车'},
+      {icon: 'heart', text: '单身'},
+      {icon: 'skin', text: '置换'},
+      {icon: 'camera', text: '摄影'},
+      {icon: 'zhihu', text: '知乎'},
+      {icon: 'taobao', text: '淘宝'},
+      {icon: 'slack', text: '交友'},
+      {icon: 'reddit', text: '育儿'},
+      {icon: 'read', text: '图书'},
+      {icon: 'customer-service', text: '音乐'},
+      {icon: 'tag', text: '其他'}
+    ];
+    const data = dataList.map(item => ({
+      icon: (<Icon size="lg"  type={item.icon} />),
+      text: item.text,
+    }));
+
+    return (
+     <div className="topic-selection">
+        <NavBar
+          className="nav"
+          mode="light"
+          leftContent="Cancel"
+          onLeftClick={() => console.log('onLeftClick')}
+          rightContent="Next" />
+
+         <WingBlank>
+          <WhiteSpace size="lg" />
+
+          <Flex align="end">
+            <Flex.Item className="avatar">
+              <Avatar src="https://ca.slack-edge.com/T95TT7UDT-WKYHKLS1H-a03025123594-512" size={80}>
+              </Avatar>
+            </Flex.Item>
+            <Flex.Item> <h3>Hello Vera!</h3></Flex.Item>
+          </Flex>
+
+          <WhiteSpace size="lg" />
+          <WhiteSpace size="lg" />
+          <WhiteSpace size="lg" />
+
+          <h3 style={{color: '#999898', fontSize: '1.25rem'}}>What's your interested topic?</h3>
+          <Grid  className="topic-grid" data={data} columnNum={3} hasLine={false} itemStyle={{color: '#fff'}} activeStyle={false} />
+
+        </WingBlank>
+      </div>
+    );
+  }
+}
+
+export default Test;
